f745c6505d52fae0b652d0923fb72e9e4021f01e
Rahulx0 <rahulsharma1234h@gmail.com>
2025-12-23 15:23:25 +0530

    Add VPC, subnet, and security group resources to main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,53 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-068c0051b15cdb816"  
-  instance_type = var.instance_type
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "BYOD3-Instance"
+    Name = "BYOD3-VPC"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "BYOD3-Subnet"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "byod3-"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "BYOD3-SecurityGroup"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-068c0051b15cdb816" 
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.example.id
+  vpc_security_group_ids = [aws_security_group.example.id]
+
+  tags = {
+    Name = "BYOD3-Example-Instance"
   }
 }
